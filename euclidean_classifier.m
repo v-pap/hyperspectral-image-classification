5a9a5788457e5f9373c430450a6244c3bb0fdd3b
v-pap <sdi1400139@di.uoa.gr>
2018-10-02 20:52:54 +0300

    restructured the project

--- euclidean_classifier.m
+++
@@ -1,8 +0,0 @@
-function [classified]=euclidean_classifier(m_hat,c,array)
-
-for i=1:size(array,2)
-    for j=1:c %Calculating the sum of distances between a pixel and the phasmatic medians of each class
-        distance(j)=(array(:,i)-(m_hat(j,:))')'*(array(:,i)-(m_hat(j,:))');
-    end
-    [~,classified(i)]=min(distance); %Assigning the pixel to the class with the minimum sum
-end
